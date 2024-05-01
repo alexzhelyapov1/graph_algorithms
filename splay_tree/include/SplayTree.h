022e9cab3e5786787abc36103c0544df1697c4aa
alex_mac <lll@>
2024-05-02 02:48:47 +0300

    working

--- splay_tree/include/SplayTree.h
+++ splay_tree/include/SplayTree.h
@@ -14,6 +14,7 @@ class Tree
     
 
     public:
+
     
 };
 
@@ -22,8 +23,17 @@ class Tree
 template <typename T>
 class Node
 {
+    firend class Tree;
+
     private:
+    T value_;
+    T* left_node_;
+    T* right_node_;
+    T* predessor_node_;
+
 
     public:
+    Node(value, predessor_ptr) : value_
+    T GetData();
     
 };
--- splay_tree/src/main.cpp
+++ splay_tree/src/main.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
+#include <string>
 #include "SplayTree.h"
+
+template <typename T>
+class Test 
+{
+  public:
+    Test(T data) {
+        data_ = data;
+        std::cout << data_ << std::endl;
+    }
+
+  private:
+    std::string data_;
+};
 
 int main() {
     std::cout << "Hello\n";
-    Test<std::string>* t = new Test<std::string>("hihihi");
-    t->Print();
+    auto t = new Test<std::string>("hihihi");
+    // t->Print();
 }
